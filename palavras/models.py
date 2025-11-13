0c21fa01554a4bc055bbe93b3e4ae86248de0527
Lucast34 <lucast34t@gmail.com>
2025-11-13 11:37:51 -0300

    Update models.py | finished palavras/

--- palavras/models.py
+++ palavras/models.py
@@ -8,7 +8,21 @@ class palavras(models.Model):
     def __str__(self):
         return "Palavra [palavra = [{}]]".format(self.palavras)
     
+
+class categoria(models.Model):
+    tipo_execoes = models.models.CharField(max_length=88, null=False,blank= False)
     
+    def __str__(self) -> str:
+        return "Categoria [categoria = [{}]]".format(self.tipo_execoes)
+
 class regras(models.Model):
+    
     tipo_uso = models.models.CharField(max_length=88, null=False,blank= False)
-    pass
+    
+    tipo_escrita = models.models.CharField(max_length=88, null=False,blank= False)
+    
+    tipo_paronimos = models.models.CharField(max_length=88, null=False,blank= False)
+    
+    tipo_homonimos = models.models.CharField(max_length=88, null=False,blank= False)
+    
+    fk_tb_categoria_id = models.models.ForeignKey(categoria, on_delete=models.CASCADE, related_name = "palavras_categoria", null = False)
