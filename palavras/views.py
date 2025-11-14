459e0fa76c41a13ca4a5fa84d331240fb70cea1b
Lucast34 <lucast34t@gmail.com>
2025-11-14 11:19:18 -0300

    Update views | updated to render index.html

--- palavras/views.py
+++ palavras/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from palavras.models import Palavras, Categoria, Regras
+
 
 # Create your views here.
+
+def index(request):
+      return render(request,'palavras/index.html')
