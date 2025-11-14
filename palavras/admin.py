82cb3974291ddf3772b6f0da024d59f2891e0776
Lucast34 <lucast34t@gmail.com>
2025-11-14 11:21:43 -0300

    Update admin.py | Finished site register

--- palavras/admin.py
+++ palavras/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+import palavras.models as pl
+
 # Register your models here.
+
+admin.site.register(pl.Palavras)
+admin.site.register(pl.Categoria)
+admin.site.register(pl.Regras)
