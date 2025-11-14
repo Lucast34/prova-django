affe63cc701d8c22d8345aebeb478268a139b717
Lucast34 <lucast34t@gmail.com>
2025-11-13 21:46:44 -0300

    Update ortografiaOficial | Some configuration start

--- orotgrafiaOficial/settings.py
+++ orotgrafiaOficial/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ortografia_oficial',
         'USER':'root',
-        'PASSWORD':'',
+        'PASSWORD':'1234',
         'HOST':'localhost',
         'PORT':'3306'
     }
